6de57a7180f4d907d917ba761880c02e79377c5d
thisjheok <jhjang3344@gmail.com>
2025-01-02 00:22:59 +0900

    [FEAT]장소상세보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DetailCourse from './Pages/DetailCourse/DetailCourse';
 import EditCourse from './Pages/EditCourse/EditCourse';
-
+import Placedetail from './Pages/Placedetail/Placedetail';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/detail-course" element={<DetailCourse />} />
-        <Route path="/edit-course" element={<EditCourse />} />
+        <Route path="/place-detail" element={<Placedetail />} />
       </Routes>
     </BrowserRouter>
   );
