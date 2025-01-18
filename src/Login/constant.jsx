ef50a5d18a900376c248737653a9ad448edabe0c
thisjheok <jhjang3344@gmail.com>
2025-01-18 18:23:15 +0900

    [FIX] 로그인 로직 수정

--- src/Login/constant.jsx
+++ src/Login/constant.jsx
@@ -1,7 +1,13 @@
+// src/constants.jsx
 export const GOOGLE_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 export const GOOGLE_SECRET_ID = import.meta.env.VITE_GOOGLE_CLIENT_SECRET;
 export const NAVER_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
 export const NAVER_SECRET_ID = import.meta.env.VITE_NAVER_CLIENT_SECRET;
 export const KAKAO_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
 export const KAKAO_SECRET_ID = import.meta.env.VITE_KAKAO_CLIENT_SECRET;
+
+// API 엔드포인트
+export const API_URL = import.meta.env.VITE_API_URL;
+
+// 리다이렉트 URI 예시
 export const REDIRECT_URI = 'https://makourse.my';
